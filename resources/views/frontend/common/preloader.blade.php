a5a1efab27d051a4433e318fee18a855018b01b3
ashu1992patel <ashu1992patel@gmail.com>
2021-09-08 02:34:17 +0530

    Loader Design Chnage

--- resources/views/frontend/common/preloader.blade.php
+++ resources/views/frontend/common/preloader.blade.php
@@ -1,7 +1,8 @@
 <div class="book_preload">
     <div class="book">
-        <div class="book__page"></div>
-        <div class="book__page"></div>
-        <div class="book__page"></div>
+        <img src="{{ url('frontend') }}/images/iqra/logo3.png" alt="" />
+        {{-- <div class="book__page"></div> --}}
+        {{-- <div class="book__page"></div> --}}
+        {{-- <div class="book__page"></div> --}}
     </div>
 </div>
